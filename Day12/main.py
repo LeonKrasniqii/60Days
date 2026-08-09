f3f2ae911a65c073ee6cf43d26c215e8f38ee1a1
LeonKrasniqii <redonpc14@gmail.com>
2026-08-09 15:49:38 +0200

    Update main.py

--- Day12/main.py
+++ Day12/main.py
@@ -266,3 +266,58 @@
 # elif 28 > temp > 0 and not is_sunny:
 #     print("It is WARMoutside !")
 #     print("It is CLOUDY")
+
+#  ------ conditional expression  = A one-line shortcut for the if-else statement(ternary operator) ------
+#                          Print or assign one of two values based on a condition
+#                          X if condition else Y
+
+# num = 6
+# a = 7
+# b = 8 
+# age = 24
+# temperature  = 30
+# user_role  = "admin"
+# print("Positive" if num > 0 else "Negative")
+# result  = "EVEN" if num % 2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num =a if a < b else b
+# status  = "Adult" if age >=  18 else "Child"
+# weather  = "Hot" if temperature > 20 else "Cold"
+# access_level = "Full Access" is user_role == "admin" else "Limited Access"
+# print(max_num)
+
+# -----    ------
+
+# name = input("Enter your full name: ")
+# phone_numer = input("Enter your phone number: ")
+# result = len(name)
+# result = name.find(" ")
+# result =  name.rfind(" ")
+# name = name.capitalize()
+# name = name.upper()
+# name = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+# result = phone_numer.count("-")
+# phone_numer = phone_numer.replace(" ","-")
+# print(phone_numer)
+
+# print(help(str))
+
+# Validate user input exercise
+# 1. username is no more than 12 characters long
+# 2. username must not contain spaces
+# 3. username must not contain digits
+
+# username = input("Enter your username: ")
+
+# if len(username) > 12:
+#     print("Your username cant be more than 12 characters")
+# elif not username.find(" ") == -1:
+#     print("Your username cant contain spaces")
+# elif not username.isalpha():
+#     print("Your username cant contain numbers")
+# else:
+#     print(f"Welcome {username}")
+
+# -----       ------
